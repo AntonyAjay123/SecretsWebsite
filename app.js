d8978b5557c6b1f64499f8f53e96dcc13b54c031
Antony Ajay Geoffrey <ajaygeoffrey456@gmail.com>
2022-04-01 21:17:38 +0530

    Add level 4 security - bcrypt

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ require('dotenv').config();
  const bodyParser= require("body-parser");
  const ejs=require("ejs");
  const encrypt = require("mongoose-encryption");
- const md5=require("md5");
+ //const md5=require("md5");
+ const bcrypt=require("bcrypt");
+ const saltRounds=10;
 
  app.use(bodyParser.urlencoded({extended:true}));
  app.set("view engine","ejs");
@@ -41,26 +43,31 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const newUser = new User({
-    email:req.body.username,
-    password:md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if(!err)
-    res.render("secrets");
-    else console.log(err);
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(!err)
+      res.render("secrets");
+      else console.log(err);
+    });
+});
 });
 
 app.post("/login",function(req,res){
   const email=req.body.username;
-  const password=md5(req.body.password);
+  const password=(req.body.password);
   User.findOne({email:email},function(err,foundUser){
     if(err)console.log(err);
     else{
       if(foundUser){
-        if(foundUser.password==password)
-        res.render("secrets");
+        bcrypt.compare(password,foundUser.password,function(err,result){
+          if(result==true)
+          res.render("secrets");
+        });
       }
     }
   });
